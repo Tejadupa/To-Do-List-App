9a0d11542766a034ceda3de3d635cfd49a3eba26
Tej <anagateja_csd236701@mgit.ac.in>
2025-09-30 10:16:49 +0530

    Add files via upload

---
+++ To-Do-List/app.js
@@ -0,0 +1,33 @@
+let todo = [];
+// let req = prompt("Enter the request");
+// console.log("req")
+let flag = true;
+while (flag) {
+    let req = prompt("Enter the request")
+    console.log(`Your request : ${req}`)
+    switch (req) {
+        case 'List':
+            let i = 0;
+            for (task of todo) {
+                console.log(i, task);
+                i += 1;
+            }
+            alert("check once all are listed below");
+            break;
+
+        case 'add':
+            let newreq = prompt("Enter the task to be added : ")
+            todo.unshift(newreq);
+            console.log(`${newreq} is added`)
+            break;
+        case 'delete':
+            let indx = Number(prompt("Enter indx : "));
+            todo.splice(indx, 1);
+            break;
+        case 'quit':
+            flag = false;
+            break
+        default:
+            alert("Entered wrong request!")
+    }
+}
